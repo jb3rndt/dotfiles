ba338ba3bcc4aee9b61e20313255b17cd3633818
jb3rndt <berndtjannis@gmail.com>
2025-01-02 23:01:19 +0100

    Add macOS defaults and application preferences

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@
 # https://github.com/webpro/dotfiles
 echo "Setting up/updating your Mac..."
 
+DOTFILES_DIR="$HOME/dotfiles"
+
 if [ ! -d "$ZSH" ]; then
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
@@ -12,7 +14,7 @@ fi
 printf "\e[32m✔ Oh my zsh installed\e[0m\n"
 
 rm -rf $HOME/.zshrc
-ln -sw $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -sw $DOTFILES_DIR/.zshrc $HOME/.zshrc
 
 printf "\e[32m✔ .zshrc linked\e[0m\n"
 
@@ -21,7 +23,7 @@ if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
 fi
 
 brew update
-brew bundle --file $HOME/dotfiles/Brewfile
+brew bundle --file $DOTFILES_DIR/Brewfile
 brew upgrade
 
 printf "\e[32m✔ Brewfile installed\e[0m\n"
@@ -30,6 +32,19 @@ source ~/.zshrc
 
 printf "\e[32m✔ Shell reloaded\e[0m\n"
 
-# Symlink preferences files
-rm -rf $HOME/Library/Preferences/com.googlecode.iterm2.plist
-ln -sw $HOME/dotfiles/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist
+# Copy preferences files and back up existing ones
+mkdir -p $DOTFILES_DIR/backup_preferences
+for file in $DOTFILES_DIR/preferences/*; do
+  filename=$(basename "$file")
+  if [ -e "$HOME/Library/Preferences/$filename" ]; then
+    cp "$HOME/Library/Preferences/$filename" "$DOTFILES_DIR/backup_preferences/"
+    rm "$HOME/Library/Preferences/$filename"
+  fi
+  cp "$file" "$HOME/Library/Preferences/"
+done
+
+printf "\e[32m✔ Preferences copied\e[0m\n"
+
+source $DOTFILES_DIR/.macos
+
+printf "\e[32m✔ macOS settings applied. Consider restarting\e[0m\n"
