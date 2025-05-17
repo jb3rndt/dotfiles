0de4aea1b03312f2d4fdeb8772248b0467efaeb9
jb3rndt <berndtjannis@gmail.com>
2025-05-17 14:54:06 +0200

    export PATH and use zsh shebang

--- aliases.zsh
+++ aliases.zsh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/zsh
 
 alias cat="bat"
 alias ls="eza --color=always --icons=always --long --git --no-filesize --no-time --no-user --no-permissions"
 alias refresh="source ~/.zshrc"
 alias reload="source ~/.zshrc"
 alias update-dotfiles="zsh $DOTFILES_DIR/update.sh"
+alias search="cat ~/.zshrc_history | grep"
