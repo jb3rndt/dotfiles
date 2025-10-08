1bd53f4212a6511c9ca0b7c0f5be4c423387fcf2
jb3rndt <berndtjannis@gmail.com>
2025-10-09 00:19:20 +0200

    add tmux

--- run.sh
+++ run.sh
@@ -16,6 +16,9 @@ printf "\e[32m✔ Oh my zsh installed\e[0m\n"
 rm -rf $HOME/.zshrc
 ln -sw $DOTFILES_DIR/.zshrc $HOME/.zshrc
 
+rm -rf $HOME/.tmux.conf
+ln -sw $DOTFILES_DIR/.tmux.conf $HOME/.tmux.conf
+
 if [ ! -f "$DOTFILES_DIR/local.zshenv" ]; then
   cp "$DOTFILES_DIR/templates/.zshenv" "$DOTFILES_DIR/local.zshenv"
 fi
@@ -30,6 +33,10 @@ fi
 
 if [ ! -d "$HOME/.nvm" ]; then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+fi
+
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 fi
 
 git config --global include.path "$DOTFILES_DIR/configs/.gitconfig"
