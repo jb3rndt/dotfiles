56d07a358fe0510279afa14c1f38e60c3117040f
jb3rndt <berndtjannis@gmail.com>
2026-03-30 18:01:13 +0200

    Remove git setup

--- setup.sh
+++ setup.sh
@@ -1,16 +1,5 @@
 #!/bin/zsh
 
-# Check if git is configured globally
-if ! git config --global user.name >/dev/null; then
-    echo "Configuring git globally..."
-    read -p "Enter your name: " name
-    read -p "Enter your email: " email
-    git config --global user.name "$name"
-    git config --global user.email "$email"
-else
-    echo "Git is already configured globally."
-fi
-
 # Clone the dotfiles repo into HOME/dotfiles
 if [ ! -d "$HOME/dotfiles" ]; then
     echo "Cloning dotfiles repository..."
