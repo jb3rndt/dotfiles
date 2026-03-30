226afcf2bcc5696e1623cdeac45c24a8f25621a9
jb3rndt <berndtjannis@gmail.com>
2026-03-30 18:01:13 +0200

    Add alias

--- aliases.zsh
+++ aliases.zsh
@@ -3,4 +3,5 @@
 alias cat="bat"
 alias ls="eza --color=always --icons=always --long --git --no-filesize --no-time --no-user --no-permissions"
 alias refresh="source ~/.zshrc"
+alias reload="source ~/.zshrc"
 alias update-dotfiles="zsh $DOTFILES_DIR/update.sh"
