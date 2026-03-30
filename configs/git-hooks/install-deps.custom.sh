bf745851a746b82d9e4f51dd0d46800ce576413f
jb3rndt <berndtjannis@gmail.com>
2026-03-30 18:01:36 +0200

    Update hooksDir, dont install deps on rebase

--- configs/git-hooks/install-deps.custom.sh
+++ configs/git-hooks/install-deps.custom.sh
@@ -3,6 +3,14 @@
 repo_root="$(git rev-parse --show-toplevel)"
 cd "$repo_root"
 
+BRANCH_NAME=$(git branch | grep '*' | sed 's/* //')
+
+# Do not run install if we're on a detached head (e.g. when rebasing)
+if [[ $BRANCH_NAME == "(no branch)" ]]; then
+	echo "🚫 Detached head detected, skipping install"
+	exit 0
+fi
+
 # Detect package manager and run install if lock file exists
 if [[ -f "yarn.lock" ]]; then
 	echo "📦 Running yarn install..."
